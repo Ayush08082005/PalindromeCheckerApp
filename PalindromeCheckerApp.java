ae75950a04188ab2d93561e75030e4b66dc8e394
Ayush08082005 <aushpes@gmail.com>
2026-03-12 10:11:28 +0530

    Ayush Raj Added UC8-IgnoreCasePalindrome

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -2,11 +2,11 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "racecar";
+        String word = "Madam";
 
         String reversed = new StringBuilder(word).reverse().toString();
 
-        if(word.equals(reversed)){
+        if(word.equalsIgnoreCase(reversed)){
             System.out.println("Palindrome");
         }else{
             System.out.println("Not Palindrome");
